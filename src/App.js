52efe279dfd045da8530881d75a26b8937d86c8e
ella-jones <14447@coderacademy.edu.au>
2023-12-09 18:19:10 +0800

    add router dom extension and set up route for about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+
 import './App.css';
 import HomePage from './pages/home/HomePage';
+import AboutPage from "./pages/about/AboutPage";
 
 function App() {
   return (
     <div className="App font-jost">
-      <HomePage />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route index path="/about" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/about/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainLayout from '../../components/MainLayout'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <MainLayout>
+            <section></section>
+        </MainLayout>
+    </div>
+  )
+}
+
+export default AboutPage

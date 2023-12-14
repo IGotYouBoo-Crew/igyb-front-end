77710063c6b400c78c8d1b7383aa17298ced6e18
Naomi Mowbray <126124875+captainaomi@users.noreply.github.com>
2023-12-15 07:57:52 +1000

    feat: events page (#14)
    * feat: initial commit for events
    
    Created events page, and skeleton for 'create an event' form
    
    * Update .env
    
    * Delete .env
    
    * feat: initial commit for events
    
    Created events page, and skeleton for 'create an event' form
    
    * Update .env
    
    * Delete .env
    
    * feat: form opens and closes
    
    * Close form by clicking outside and formatting
    
    * feat: styling and added date picker
    
    Need to revisit tomorrow but excited with progress
    
    * fix: edited out unused code for warnings

--- src/components/events/EventForm.jsx
+++ src/components/events/EventForm.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 import { createEvent } from './createEvent';
 
-const EventForm = () => {
+
+const EventForm = ({ isVisible, onClose }) => {
     const [title, setTitle] = useState("");
     const [date, setDate] = useState("");
     const [content, setContent] = useState("");
@@ -11,49 +16,96 @@ const EventForm = () => {
 //     const [finish, setFinish] = useState("");
 //     const [ticketLink, setTicketLink] = useState("");
     
-    const submitHandler = (event) => {
+    const handleClose = (event) => {
+        if (event.target.id === 'eventForm') onClose();
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log('submit');
-    }
+        // Need to add validation for form fields
 
+        try {
+            await createEvent(title, date, content);
+
+            // Clear the form fields after submission
+            setTitle('');
+            setDate('');
+            setContent('');
+
+            onClose();
+        } catch (error) {
+            console.log('Looks like we have a problem:', error);
+        }
+    };
+    
+  if (!isVisible) return null;
   return (
-    <form onSubmit={submitHandler}>
-      <div className="Event text-sea">
-        <label>Title:</label>
-        <input 
-            type="text" 
-            name="titleInput" 
-            id="titleInput" 
-            placeholder="EVENT TITLE*"
-            value={title} 
-            onChange={(event) => setTitle(event.target.value)} 
-        />
+    <div 
+        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex flex-col items-center justify-center"
+        id="eventForm"
+        onClick={handleClose}
+    >
 
-        <label>Date:</label>
-        <input 
-            type="text" 
-            name="dateInput" 
-            id="dateInput" 
-            placeholder="EVENT DATE (DD/MM/YYY)*"
-            value={date} 
-            onChange={(event) => setDate(event.target.value)} 
-        />
+        <div className="p-6 lg:p-8 bg-honey text-white rounded-3xl">
+            <div className="flex justify-items-end"> 
+                <button 
+                    className="text-white text-xl text-bold" 
+                    onClick={() => onClose()}
+                >
+                    X
+                </button>
+            </div>
+            <h1 className="mt-2 font-bold text-2xl md:text-3xl text-center ">
+                ENTER YOUR EVENT DETAILS
+            </h1>
+            <form onSubmit={handleSubmit} className="flex flex-col py-5">
+                <label></label>
+                <input 
+                    type="text" 
+                    name="titleInput" 
+                    id="titleInput" 
+                    placeholder="EVENT TITLE*"
+                    value={title} 
+                    onChange={(event) => setTitle(event.target.value)} 
+                    className="placeholder:font-bold text-black text-sm md:text-base placeholder:text-sm md:placeholder:text-base placeholder:text-[#959EAD] rounded-3xl pl-5 pr-3 py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]" 
 
-        <label>Content:</label>
-        <input 
-            type="text" 
-            name="contentInput" 
-            id="contentInput" 
-            placeholder="WRITE A SHORT DESCRIPTION OF YOUR EVENT*"
-            value={content} 
-            onChange={(event) => setContent(event.target.value)} 
-        />
-                
-        <button onClick={() => {createEvent(title, date, content)}}>
-            SUBMIT
-        </button>
-      </div>
-    </form>
+                />
+
+                <label></label>
+                <input 
+                    type="text" 
+                    name="dateInput" 
+                    id="dateInput" 
+                    placeholder="EVENT DATE (DD/MM/YYY)*"
+                    value={date} 
+                    onChange={(event) => setDate(event.target.value)} 
+                    className="placeholder:font-bold text-black text-sm md:text-base placeholder:text-sm md:placeholder:text-base placeholder:text-[#959EAD] rounded-3xl pl-5 pr-3 py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]" 
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker />
+                </LocalizationProvider>
+
+                <label></label>
+                <input 
+                    type="text" 
+                    name="contentInput" 
+                    id="contentInput" 
+                    placeholder="WRITE A SHORT DESCRIPTION OF YOUR EVENT*"
+                    value={content} 
+                    onChange={(event) => setContent(event.target.value)} 
+                    className="placeholder:font-bold text-black text-sm md:text-base placeholder:text-sm md:placeholder:text-base placeholder:text-[#959EAD] rounded-3xl pl-5 pr-3 py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]" 
+
+                />
+                        
+                <button 
+                    type="submit"
+                    className="border border-white rounded-3xl px-5 py-2 text-white text-sm md:text-base" 
+                >
+                    SUBMIT
+                </button>
+            </form>
+        </div>
+    </div>
   );
 };
 
--- src/components/events/EventsFormContainer.jsx
+++ src/components/events/EventsFormContainer.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EventForm from './EventForm'
 
 const EventsFormContainer = () => {
-  return (
-    <div>
-      <EventForm />
-    </div>
-  )
+  const [showEventForm, setShowEventForm] = useState(false);
+
+    return (
+      <div className="flex flex-col">
+        <h2 className='text-center md:text-right mt-10 md:mt-6 font-bold'>GOT AN UPCOMING EVENT?</h2>
+        <button 
+          className='bg-honey text-white rounded-3xl px-6 py-2' 
+          onClick={() => setShowEventForm(true)}>
+          CREATE AN EVENT
+        </button>
+        <EventForm 
+          isVisible={showEventForm}
+          onClose={() => setShowEventForm(false)} 
+        >
+          <div></div>
+        </EventForm>
+      </div>
+      
+
+    )
 }
 
-export default EventsFormContainer
+export default EventsFormContainer;

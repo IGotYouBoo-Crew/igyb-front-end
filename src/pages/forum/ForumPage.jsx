6a53a945294e24ff8bb9a2bdb49d00451012a2e2
ella-jones <14447@coderacademy.edu.au>
2023-12-14 15:40:16 +0800

    feat: set up route for Forum page and create hero section

---
+++ src/pages/forum/ForumPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainLayout from '../../components/MainLayout'
+import Hero from './container/Hero'
+
+const ForumPage = () => {
+  return (
+    <MainLayout>
+        <section>
+            <Hero />
+        </section>
+    </MainLayout>
+  )
+}
+
+export default ForumPage

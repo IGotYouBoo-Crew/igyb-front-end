34b1018e5116881ebe9965295cba23cf3cee6441
ella-jones <14447@coderacademy.edu.au>
2023-12-17 11:34:20 +0800

    fix: styling of post button and search bar

--- src/components/posts/PostFormContainer.jsx
+++ src/components/posts/PostFormContainer.jsx
@@ -8,7 +8,7 @@ const PostFormContainer = () => {
       <div className="flex flex-col">
         <h2 className='text-center md:text-right mt-10 md:mt-6 font-bold'>GOT SOMETHING TO SAY?</h2>
         <button 
-          className='bg-periwinkle text-white rounded-3xl px-6 py-2 mt-2' 
+          className='bg-periwinkle text-white font-bold text-xl rounded-3xl px-6 py-2 mt-2' 
           onClick={() => setShowPostForm(true)}>
           CREATE A POST
         </button>
--- src/pages/forum/container/Hero.jsx
+++ src/pages/forum/container/Hero.jsx
@@ -18,23 +18,24 @@ const Hero = () => {
     //                     ullamco laboris nisi ut aliquip ex ea.
     //                 </p>
     //             </div>
-    <section className="relative bg-honey">
-          <img className='z-10 relative -mt-6 w-full' src={images.ForumImage} alt="Forum Header" />
-          <div className='bg-honey rounded-3xl z-40 px-10 lg:px-20 py-10 text-white relative -mt-6 flex flex-col items-center md:flex-row md:items-start md:justify-between'>
-              <div className="md:w-1/2 flex flex-col">
-                  <h2 className='text-center md:text-left md:mt-6 font-bold'>NEED SOME ADVICE?</h2>
-                  <h1 className='mt-2 font-bold text-2xl md:text-3xl text-center md:text-left'>WELCOME TO THE FORUM</h1>
-                  <p className='mt-4 text-center md:text-left'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-                      irure dolor in reprehenderit in voluptate. 
-                  </p>
-              </div>
-                <PostFormContainer />
+    <section className="relative bg-honey pb-10">
+        <img className='z-10 relative -mt-6 w-full' src={images.ForumImage} alt="Forum Header" />
+        <div className='px-10 bg-honey rounded-t-3xl z-30 lg:px-20 py-10 text-white relative -mt-6 flex flex-col items-center md:flex-row md:items-start md:justify-between'>
+            <div className="md:w-1/2 flex flex-col">
+                <h2 className='text-center md:text-left md:mt-6 font-bold'>NEED SOME ADVICE?</h2>
+                <h1 className='mt-2 font-bold text-2xl md:text-3xl text-center md:text-left'>WELCOME TO THE FORUM</h1>
+                <p className='mt-4 text-center md:text-left'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                    irure dolor in reprehenderit in voluptate. 
+                </p>
             </div>
 
-            <div className='flex flex-col gap-y-2.5 mt-12 relative md:w-3/4 md:left-[12.5%] lg:w-2/3 lg:left-[16.67%] '>
+            <PostFormContainer />
+        </div>
+        <div className='px-6 md:px-0'>
+            <div className='flex flex-col gap-y-2.5 mt-12 relative md:w-3/4 md:left-[12.5%] lg:w-2/3 lg:left-[16.67%]'>
                 <div className='relative'>
                     <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]'/>
                     <input 
@@ -43,11 +44,12 @@ const Hero = () => {
                         placeholder='SEARCH FORUM POSTS' 
                     />
                 </div>
-                <button className='bg-periwinkle text-white text-sm md:text-base font-bold rounded-r-3xl px-3 md:px-8 absolute right-0 top-1/2 -translate-y-1/2 w-fit py-3 '>
+                
+                <button className='bg-periwinkle text-white text-sm md:text-base font-bold rounded-r-3xl px-3 md:px-8 absolute right-0 top-1/2 -translate-y-1/2 w-fit py-3'>
                     SEARCH
                 </button>
             </div>
-        
+        </div>
     </section>
   )
 }

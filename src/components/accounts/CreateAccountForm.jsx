6a25832606cdc6a12e2ebe3b7ada6586eeb995e4
JackDixon019 <13709@coderacademy.edu.au>
2023-12-18 14:14:29 +1100

    fix: removed unused import

--- src/components/accounts/CreateAccountForm.jsx
+++ src/components/accounts/CreateAccountForm.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../../contexts/UserContext";
-import { buttonStyle, inputStyle } from "../../constants/styles";
+import { buttonStyle } from "../../constants/styles";
 import colourways from "../../constants/colourways";
 import FormInput from "../FormInput";
 import ErrorMessage from "../ErrorMessage";

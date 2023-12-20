5f7246cf88ba5c1395995314e8af1c53f26cb7a6
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-12-21 01:48:31 +1000

    trying to format large screen size for events correctly

--- src/pages/events/container/EventsList.jsx
+++ src/pages/events/container/EventsList.jsx
@@ -1,57 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import EventCard from '../../../components/events/EventCard';
-
+import React, { useEffect, useState } from "react";
+import EventCard from "../../../components/events/EventCard";
 
 const backendURL = process.env.REACT_APP_BACKEND_URL;
 
 const EventsList = () => {
-
   let [listOfEvents, setListOfEvents] = useState([]);
 
   useEffect(() => {
-      getAllEvents()
+    getAllEvents();
   }, []);
-  
+
   async function getAllEvents() {
-      let response = await fetch(backendURL + "/events/", {
-          method: "GET",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          credentials: "include",
-      });
-      
-      const responseData = await response.json();
-  
-      console.log(responseData);
-      setListOfEvents(responseData);
-      return responseData;
+    let response = await fetch(backendURL + "/events/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+    setListOfEvents(responseData);
+    return responseData;
   }
 
   return (
-    <section className='-mt-1 px-6 md:px-20 lg:px-36 py-10 bg-indigo rounded-b-3xl relative'>
-        <div className='flex flex-col gap-y-7 md:gap-y-9 lg:gap-y-12'>
-            {
-                listOfEvents ?
-                listOfEvents.map((event, index) => {
-                    return (
-                        <EventCard 
-                            eventData={event} 
-                            key={index} 
-                            className='w-full' 
-                        />
-                    )
-                })
-                :
-                ""
-            }
-//         </div>
-//         <button className='mx-auto flex items-center gap-x-2 font-bold text-white border-2 border-white px-10 py-2 mt-12 mb-3 rounded-3xl uppercase'>
-//             Load more
-//         </button>
-
-//     </section>
-  )
-}
+    <section className="-mt-1 px-6 md:px-20 lg:px-36 py-10 bg-indigo rounded-b-3xl relative">
+      <div className="flex flex-col gap-y-7 md:gap-y-9 lg:gap-y-12">
+        {listOfEvents
+          ? listOfEvents.map((event, index) => {
+              return (
+                <EventCard eventData={event} key={index} className="w-full" />
+              );
+            })
+          : ""}
+      </div>
+      <button className="mx-auto flex items-center gap-x-2 font-bold text-white border-2 border-white px-10 py-2 mt-12 mb-3 rounded-3xl uppercase">
+        Load more
+      </button>
+    </section>
+  );
+};
 
 export default EventsList;
--- src/pages/events/container/SingleEventContainer.jsx
+++ src/pages/events/container/SingleEventContainer.jsx
@@ -29,47 +29,32 @@ const SingleEventContainer = () => {
   }
 
   return (
-    <section className="bg-indigo bg-cover bg-center w-screen rounded-3xl relative flex flex-col pb-5 px-8 lg:px-56 text-center text-white justify-center items-center">
+    <section className="flex-1 bg-indigo rounded-3xl relative flex flex-col pb-5 px-8 text-center text-white">
       {event ? (
         <>
-          <section className="lg:pb-20 text-center lg:flex-row-reverse lg:justify-between items-center lg:items-start lg:text-left">
-              <img
-                src={event.image}
-                alt="event image"
-                className="rounded-3xl mt-2 object-cover w-full h-auto lg:w-1/2"
-              />
-            <div className="flex flex-col justify-center items-center h-full lg:items-start md:px-20 lg:px-12 lg:pb-10">
-              <h1 className="text-3xl font-bold lg:text-4xl pt-10 capitalize">
-                {event.title}
-              </h1>
-              <h3 className="text-md text-white uppercase">
-                {event.date}
-              </h3>
-              <h3 className="text-md text-white uppercase">
-                {event.start} - {event.finish}
-              </h3>
+          <section className="text-center lg:flex-row-reverse lg:justify-between items-center lg:items-start lg:text-left lg:pb-10 lg:px-12">
+            <img
+            src={event.image}
+            alt="event image"
+            className="rounded-3xl mt-2 object-cover w-full h-auto lg:w-1/2"/>
+            <div className="mt-6 text-center lg:text-left">
+              <h1 className="text-3xl font-bold lg:text-4xl pt-10 capitalize">{event.title}</h1>
+              <h3 className="text-md text-white uppercase">{event.date}</h3>
+              <h3 className="text-md text-white uppercase">{event.start} - {event.finish}</h3>
               <div className="text-white mt-6 text-center lg:text-left">
                 <div className="text-sm md:text-base px-4 md:px-0 lg:pl-0 lg:pr-16 py-3">
                   <p>{event.content}</p>
-                  <h4 className="text-sm font-bold text-white opacity-70 md:px-20 lg:px-12 mt-4">
-                    Event created by @
-                    {event.author.username || " one of our Superstars!"}
-                  </h4>
+                  <h4 className="text-sm font-bold text-white opacity-70 md:px-20 lg:px-12 mt-4">Event created by @{event.author.username || " one of our Superstars!"}</h4>
                 </div>
               </div>
             </div>
           </section>
           {event.ticketLink ? (
-              <a 
-                target="_blank"
-                href={event.ticketLink}>
-              <button
-                rel="noreferrer"
-                className="bg-white text-indigo font-bold rounded-3xl px-6 py-2 my-5 uppercase"
-                >
-                Tickets/RSVP
-              </button>
-            </a>
+              <a target="_blank"href={event.ticketLink}>
+                <button rel="noreferrer" className="bg-white text-indigo font-bold rounded-3xl px-6 py-2 my-5 uppercase">
+                    Tickets/RSVP
+                </button>
+              </a>
           ) : null}
           <div className="flex flex-row justify-between text-white text-xs md:text-base pb-5 ">
             <button className="border-2 border-white font-bold rounded-3xl px-6 py-2 mt-2 uppercase">

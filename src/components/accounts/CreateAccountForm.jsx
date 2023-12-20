ad335b8defcdde2306333840f426f1e06ed82b6f
JackDixon019 <13709@coderacademy.edu.au>
2023-12-20 18:11:12 +1100

    Feat: adding password checking

--- src/components/accounts/CreateAccountForm.jsx
+++ src/components/accounts/CreateAccountForm.jsx
@@ -14,13 +14,56 @@ export default function CreateAccountForm() {
 
     async function handleClick(event) {
         event.preventDefault();
-        let newData = { username: "", password: "" };
-        newData.username = document.getElementsByName("username")[0].value;
-        newData.password = document.getElementsByName("password")[0].value;
-        newData.email = document.getElementsByName("email")[0].value.toLowerCase();
-        newData.pronouns = document.getElementsByName("pronouns")[0].value;
+        let newData = {};
+        inputs.forEach((inputName) => {
+            newData[inputName] = document.getElementsByName(inputName)[0].value;
+        });
+        newData.email = newData.email.toLowerCase();
+        // console.log(newData.password)
+        // console.log(passwordCheckFailed(newData.password))
+        // if(passwordCheckFailed(newData.password)){
+        //     console.log("flag inside if check")
+        //     return
+        // }
+        // console.log("flag after if check")
         setData(newData);
     }
+
+    // function passwordCheckFailed(password) {
+    //     console.log(password)
+    //     console.log(password.length)
+    //     let passwordRequirements = [
+    //         {
+    //             errorMessage: "Password length must be greater than 8",
+    //             test: password.length > 8,
+    //         },
+    //         {
+    //             errorMessage: "Password must contain at least one uppercase letter",
+    //             test: password.match(/[A-Z]+/g) || true,
+    //         },
+    //         {
+    //             errorMessage: "Password must contain at least one number",
+    //             test: password.match(/[0-9]/) || true,
+    //         },
+    //     ];
+    //     let pass = false
+    //     for (let index = 0; index < passwordRequirements.length; index++) {
+    //         const requirement = array[index];
+    //         console.log(requirement.errorMessage + ", " + requirement.test)
+    //         if(!requirement.test){
+    //             setError(requirement.errorMessage)
+    //             return
+    //         } else {
+    //             pass = true
+                
+    //         }
+
+            
+    //     }
+    //     passwordRequirements.forEach(requirement => {
+    //     });
+    //     return pass
+    // }
 
     useEffect(() => {
         if (data.username.length > 0) {
@@ -41,8 +84,8 @@ export default function CreateAccountForm() {
         const responseData = await response.json();
         if (responseData.errors) {
             if (responseData.errors.split(" ")[0] === "E11000") {
-                let key = responseData.errors.split("key: {")[1].split(":")[0]
-                responseData.errors = `The ${key} entered is already associated with an account. \nPlease log in to the account, or try another ${key}`
+                let key = responseData.errors.split("key: {")[1].split(":")[0];
+                responseData.errors = `The ${key} entered is already associated with an account. \nPlease log in to the account, or try another ${key}`;
             }
             setError(responseData.errors);
             return;
@@ -52,6 +95,7 @@ export default function CreateAccountForm() {
     }
 
     const inputs = ["username", "password", "email", "pronouns"];
+
     return (
         <form className="flex flex-col mb-2">
             {inputs.map((value, i) => {

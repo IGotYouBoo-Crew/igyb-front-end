f4b59effeea9ebb19aa0fbc242e74ae5daf6f486
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-12-23 11:41:25 +1000

    rebasing

--- src/components/events/updateEvent.js
+++ src/components/events/updateEvent.js
@@ -1,5 +1,5 @@
-async function updateEvent(title, host, image, start, finish, ticketLink, content, date, event) {
-    console.log(title, host, image, start, finish, ticketLink, content, date, event);
+async function updateEvent(title, host, image, start, finish, ticketLink, content, event) {
+    console.log(title, host, image, start, finish, ticketLink, content, event);
 
     let result = await fetch(
         process.env.REACT_APP_BACKEND_URL + "/events/" + event._id + "/" + event.author._id,

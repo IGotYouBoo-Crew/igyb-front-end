e51c2513bc929b07c8f5de6c0b22ee5e028931ae
JackDixon019 <13709@coderacademy.edu.au>
2023-12-23 11:21:10 +1100

    Just added the shadow that I missed the first time oops

--- src/pages/forum/container/Hero.jsx
+++ src/pages/forum/container/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FiSearch } from "react-icons/fi"
 import { images } from '../../../constants'
 import PostFormContainer from '../../../components/posts/PostFormContainer'
+import SearchBar from '../../../components/SearchBar'
 
 const Hero = () => {
   return (
@@ -20,22 +21,7 @@ const Hero = () => {
             </div>
             <PostFormContainer />
         </div>
-        <div className='px-6 md:px-0'>
-            <div className='flex flex-col gap-y-2.5 mt-12 relative md:w-3/4 md:left-[12.5%] lg:w-2/3 lg:left-[16.67%]'>
-                <div className='relative'>
-                    <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]'/>
-                    <input 
-                        className='placeholder:font-bold placeholder:uppercase text-black text-sm md:text-base placeholder:text-sm md:placeholder:text-base placeholder:text-[#959EAD] rounded-3xl pl-12 pr-3 w-full py-3 focus:outline-non shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]' 
-                        type="text"
-                        placeholder='Search Forum Posts' 
-                    />
-                </div>
-                
-                <button className='bg-periwinkle text-white uppercase text-sm md:text-base font-bold rounded-r-3xl px-3 md:px-8 absolute right-0 top-1/2 -translate-y-1/2 w-fit py-3'>
-                    Search
-                </button>
-            </div>
-        </div>
+        <SearchBar />
     </section>
   )
 }

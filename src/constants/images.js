2eddfea76a2e2b3f944d2719d78a16cd83aeb4e0
ella-jones <14447@coderacademy.edu.au>
2023-12-24 14:20:31 +0800

    fix: Header image sizes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,26 +1,20 @@
 import Logo from "../assets/Logo.png";
-import HomeHeader from "../assets/HomeHeader.png";
-import EventsHero from "../assets/EventsHero.png";
-import Post1Image from "../assets/posts/post1.png";
-import PostProfile1 from "../assets/posts/post-profile1.jpg";
-import AboutImage from "../assets/AboutImage.png";
-import ContactImage from "../assets/ContactImage.png";
-import EventHeader from "../assets/EventsHeader.png";
-import ForumImage from "../assets/ForumImage.png";
-import CatImage from "../assets/kitty.webp";
+import HomeHeader from "../assets/HomeHeader2.png";
+import EventsHero from "../assets/EventsHero2.png";
+import AboutImage from "../assets/AboutImage2.png";
+import ContactImage from "../assets/ContactImage2.png";
+import EventHeader from "../assets/EventsHeader2.png";
+import ForumImage from "../assets/ForumImage2.png";
 import ProfileDefault from "../assets/posts/profileDefault.jpeg";
 
 const images = {
     Logo,
     HomeHeader,
     EventsHero,
-    Post1Image,
-    PostProfile1,
     AboutImage,
     ContactImage,
     EventHeader,
     ForumImage,
-    CatImage,
     ProfileDefault,
 };
 

08e91686c0b8a21f005d31b5ae51d6f0f19b0172
ella-jones <14447@coderacademy.edu.au>
2023-12-24 11:20:21 +0800

    testing if post context is needed

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
-import { PostProvider } from "./contexts/PostContext";
+// import { PostProvider } from "./contexts/PostContext";
 import { SearchUserProvider } from "./contexts/SearchUserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <UserProvider>
-      <PostProvider>
+      {/* <PostProvider> */}
         <SearchUserProvider>
           <App />
         </SearchUserProvider>
-      </PostProvider>
+      {/* </PostProvider> */}
     </UserProvider>
   </BrowserRouter>
 );
